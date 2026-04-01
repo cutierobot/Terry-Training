436dffed56d59d4de2fdecb770055b7446c5c75c
Mikaela Donovan <mikaelasomerville@gmail.com>
2026-04-01 21:03:23 +1000

    this weeks update

--- TerryTraining.API/Program.cs
+++ TerryTraining.API/Program.cs
@@ -13,6 +13,23 @@ using TerryTraining.Persistence.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
+/*
+ *---------------------------
+ * TODO/WHAT I DID LAST TIME
+ *---------------------------
+ * Previously learnt about Aggreggates.
+ * figured out Order and OrderLines are. A customer creates one single order, that order will have multiple
+ * OrderLine's (one for each product in the Order)
+ * Learned about ValueObjects. Basically a thing where they don't need to be unique as the data is unique enough. such as a address.
+ * Can be created with either record of base class
+ *
+ *---------------------------
+ * TODO NEXT WEEK/WHERE IM UP DO
+ *---------------------------
+ * Start creating an writing code for Order Aggregate using knowledge of valueObject and Aggregates. Ask Terry
+ * if should use record or base class
+ */
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -87,7 +104,6 @@ app.MapPut("/product/new", async (string name, string description, int stockcoun
          return result == null ? Results.NotFound() : Results.Created($"/product/{result.Value.Id}", result);
     })
     .WithName("NewProduct")
-    // .WithTags("Not Implemented")
     .WithOpenApi(x => new OpenApiOperation(x)
     {
         Summary = "Create a new product",
@@ -114,6 +130,82 @@ app.MapPut("/product/{productid}/stock", async (int productid, int stockcount, I
         Description = "Adds stock to a product with provided Id",
     });
 
+// CreateCustomer(givenName, surname, addressDetails)
+app.MapPut("/customer/new", async (string givenName, string surname, string addressDetails/*, IProductService productService*/) =>
+    {
+      // 1. create a customerDTO
+      // 2. create a customerService
+      // 3. add to unit of work.???
+    })
+    .WithName("CreateCustomer")
+    .WithTags("Not Implemented")
+    .WithOpenApi(x => new OpenApiOperation(x)
+    {
+        Summary = "Creates a customer",
+        Description = "Creates a customer from name and address details",
+    });
+
+// CreateOrder(customerId, orderLines)
+// ask Terry about this one what orderLines is
+app.MapPut("/order/new", async (int customerId/*, IProductService productService*/) =>
+    {
+        // Products exist
+        // Ensure customer exists
+        // Ensure stock available
+        // Reserve stock
+    })
+    .WithName("CreateOrder")
+    .WithTags("Not Implemented")
+    .WithOpenApi(x => new OpenApiOperation(x)
+    {
+        Summary = "Creates a order",
+        Description = "Not really sure on this one to ask Terry about",
+    });
+
+
+// FullfillOrder(orderId)
+app.MapPut("/order/update/{orderId}", async (int orderId/*, IOrderService orderService*/) =>
+    {
+        // Ensure order exists
+        // Update stock levels and reserve levels
+        // Set order completed
+    })
+    .WithName("FullfillOrder")
+    .WithTags("Not Implemented")
+    .WithOpenApi(x => new OpenApiOperation(x)
+    {
+        Summary = "Updates a Order",
+        Description = "Updates a Order ensuring stock availability, stocks levels updated after order and that if order can be completed successfully it is marked as such",
+    });
+
+
+// GetCustomer(id)
+app.MapGet("/customer/{id}", async (int id/*, IProductService productService*/) =>
+    {
+        // 1.
+    })
+    .WithName("GetCustomer")
+    .WithTags("Not Implemented")
+    .WithOpenApi(x => new OpenApiOperation(x)
+    {
+        Summary = "Retrieve a customer by their Id",
+        Description = "Retrieves a customer using it's unique Id"
+    });
+
+// GetOrder(orderId)
+app.MapGet("/order/{orderId}", async (int orderId/*, IOrderService orderService*/) =>
+    {
+        // Ensure order exists
+        // Return all details except customer details
+    })
+    .WithName("GetOrder")
+    .WithTags("Not Implemented")
+    .WithOpenApi(x => new OpenApiOperation(x)
+    {
+        Summary = "Retrieve a order by it's Id",
+        Description = "Retrieves a Order places using it's unique Id"
+    });
+
 app.MapGet("/product/{id}", async (int id, IProductService productService) =>
     {
         var result = await productService.GetProduct(id);
@@ -125,10 +217,5 @@ app.MapGet("/product/{id}", async (int id, IProductService productService) =>
         Summary = "Retrieve a product from it's Id",
         Description = "Retrieves a product using it's db Id, this one is intended for testing connection if anything"
     });
-
-// Check doesn’t already exist
-// Check name, description sizes
-// Check stockcount > 0
-
 
 app.Run();
